9cdedb4faeddad4b864e7c6611e7b90dc94b23ee
Vivek Gupta <vivg269@gmail.com>
2022-02-28 15:05:53 +1100

    Removed the tests dir

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(  name = 'Visibility_injector',
         version = '1.0',
-        packages = ['Visibility_injector',
-                    'Visibility_injector.tests'],
+        packages = ['Visibility_injector'],
+#                    'Visibility_injector.tests'],
         scripts = ['bin/plot_fake_injections.py'],
         install_requires = ['numpy', 'matplotlib', 'pyyaml', 'Furby_p3']
     )
