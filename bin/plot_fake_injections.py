eb705cd67b03a65ec1822de6a905bcc301589235
Vivek Gupta <vivg269@gmail.com>
2022-03-01 14:43:22 +1100

    Major changes after Keith's request

--- bin/plot_fake_injections.py
+++ bin/plot_fake_injections.py
@@ -3,17 +3,44 @@ from Visibility_injector.inject_in_fake_data import FakeVisibility
 import numpy as np
 import argparse
 
-class FakePlan():
-    pass
+class FakePlan(object):
+    
+    def __init__(self, nt=300, nbl=190, tsamp=0.001, freqs = np.arange(735, 991, 1) + 0.5):
+        self.nt = nt
+        self.nbl = nbl
+        self.freqs = freqs
+        self.tsamp = tsamp
 
-plan = FakePlan
-plan.nt = 300
-plan.nf = 256
-plan.nbl = 190
-#The following three params are needed for furby simulation
-plan.ftop = 991
-plan.fbottom = 735
-plan.tsamp = 0.001
+    @property
+    def nf(self):
+        '''
+        Returns the number of channels
+        '''
+        return len(self.freqs)
+        
+    @property
+    def foff(self):
+        '''
+        Returns the channel width (in MHz)
+        '''
+        return np.abs(self.freqs[1] - self.freqs[0])
+
+    @property
+    def fmax(self):
+        '''
+        Returns the max freq (in MHz)
+        '''
+        return self.freqs[-1]
+
+    @property
+    def fmin(self):
+        '''
+        Returns the minimum freq (in MHz)
+        '''
+        return self.freqs[0]
+
+
+plan = FakePlan()
 
 def main(args):
     injections_params_file = args.inj_params
