6e040cdc8c836f60f345e3a270a9ba8e8ce0ea18
Vivek Gupta <vivg269@gmail.com>
2022-05-31 04:59:21 +1000

    Merged plot_fake_injections with plot_vis_injections

--- bin/plot_fake_injections.py
+++ bin/plot_fake_injections.py
@@ -2,49 +2,21 @@ import matplotlib.pyplot as plt
 from Visibility_injector.inject_in_fake_data import FakeVisibility
 import numpy as np
 import argparse
-
-class FakePlan(object):
-    
-    def __init__(self, nt=300, nbl=190, tsamp=0.001, freqs = 1e6 * (np.arange(735, 991, 1) + 0.5)):
-        self.nt = nt
-        self.nbl = nbl
-        self.freqs = freqs
-        self.tsamp_s = tsamp
-
-    @property
-    def nf(self):
-        '''
-        Returns the number of channels
-        '''
-        return len(self.freqs)
-
-    @property
-    def foff(self):
-        '''
-        Returns the channel width (in MHz)
-        '''
-        return np.abs(self.freqs[1] - self.freqs[0])
-
-    @property
-    def fmax(self):
-        '''
-        Returns the max freq (in MHz)
-        '''
-        return self.freqs[-1]
-
-    @property
-    def fmin(self):
-        '''
-        Returns the minimum freq (in MHz)
-        '''
-        return self.freqs[0]
+from craft.craco_plan import PipelinePlan
+from craft import uvfits
 
 
-plan = FakePlan()
 
 def main(args):
     injections_params_file = args.inj_params
+    fits = '/home/gup037/tmp/frb_d0_t0_a1_sninf_lm00.fits'
+    f = uvfits.open(fits) 
+    plan = PipelinePlan(f, f"--ndm 512")
+
+    injections_params_file = args.inj_params
+
     FV = FakeVisibility(plan, tot_nsamps = None,  injection_params_file = injections_params_file)
+    
     block_gen = FV.get_fake_data_block
 
     for ii, block in enumerate(FV.get_fake_data_block()):
