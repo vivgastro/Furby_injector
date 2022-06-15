d9a9bf7847315c03aa1f15cb4afd7f608ea691a5
Vivek Gupta <gup037@athena.atnf.csiro.au>
2022-06-15 14:38:55 +1000

    Added VG's version of dm_mask fx

--- bin/test_fake_injecitons.py
+++ bin/test_fake_injecitons.py
@@ -2,7 +2,10 @@ from craft.craco_plan import PipelinePlan
 from craft import uvfits
 import sys
 sys.path.append("/home/gup037/Codes/craft/src/craft")
+
 from get_dm_mask import get_dm_mask
+from Furby_p3.miniFDMT import get_dm_mask as get_dm_mask_vg
+
 from craft import fdmt
 
 import matplotlib.pyplot as plt
@@ -62,10 +65,12 @@ def main(args):
 
     injections_params_file = args.inj_params
     plan = PipelinePlan(f, "--ndm 512")
+    print("Plan.freqs = ", plan.freqs)
     FV = FakeVisibility(plan, tot_nsamps = 256 * 200 * 50,  injection_params_file = args.inj_params)
     sums_m1 = []
     sums_0 = []
     sums_1 = []
+    sums_1_vg = []
     true_sums = []
     dms = []
     sub=0
@@ -90,65 +95,69 @@ def main(args):
                 #print("Sum of the data in block {1} = {0}".format(np.sum(block.real[:, :, -4]), ii))
                 #get_dm_mask_call = f"get_dm_mask((f_lower = {(plan.fmin - np.abs(plan.foff)/2.)*1e-9},  chanbwGhz={np.abs(plan.foff)*1e-9}, Nchan={plan.nf}, NDMtrials = 1024, TimeBlockSize = {plan.nt}, wanted_idm = {dm_samps})"
                 dm_mask = get_dm_mask(f_lower = (plan.fmin - np.abs(plan.foff)/2.)*1e-9,  chanbwGhz=np.abs(plan.foff)*1e-9, Nchan=plan.nf, NDMtrials = 1024, TimeBlockSize = plan.nt, wanted_idm = int(dm_samps))
+                dm_mask_vg = get_dm_mask_vg(int(dm_samps), plan.freqs[::-1])[::-1]
+
                 if args.plot_all:
+                    '''
+                    software FDMT
                     vfdmt = fdmt.Fdmt((plan.fmin - np.abs(plan.foff)/2.)*1e-9,  np.abs(plan.foff)*1e-9, plan.nf, 1024, iblock.shape[2])
                     dout = vfdmt(np.abs(iblock).sum(axis=0))
                     print("sum= ", np.abs(iblock).sum())
+                    '''
+
+                    '''
+                    Custom dedispersion
                     plt.figure()
                     plt.imshow(dedisperse(np.abs(iblock).sum(axis=0), dm_samps = -300), aspect='auto', interpolation="None")
                     plt.title(f"Dedispersed block at De-DM = -300.0")
+                    '''
                     
-                    plt.figure()
-                    plt.plot(dedisperse(np.abs(iblock).sum(axis=0), dm_samps = -300).sum(axis=0))
-                    print(dout.shape, ',,,,,,')
-                    plt.plot(dout[300, :], label="DM_out")
-                    plt.title("Comparison of freq averaged time series after\nde-dispersion (simulator vs python pipeline)")
-                    
-                    plt.legend()
+                    '''
+                    Butterfly plot
 
                     plt.figure()
                     plt.imshow(dout, aspect='auto', interpolation="None")
                     plt.title("Butterfly plot from the python pipeline")
-
-                    plt.figure()
-                    plt.imshow(np.abs(iblock).sum(axis=0), aspect='auto', interpolation = "None")
-                    plt.title("Input block")
+                    '''
 
                 nsamps_in_mask = dm_mask.shape[1]
+                nsamps_in_mask_vg = dm_mask_vg.shape[1]
+
                 frb_block = np.abs(iblock).sum(axis=0)
                 frb_loc = np.argmax(frb_block[0])
                 print(ii, iblock.shape)
                 print("shape of dm_mask = ", dm_mask.shape, frb_loc)
-                frb_ex_m1 = frb_block[:,frb_loc - nsamps_in_mask-1:frb_loc-1]
-                frb_ex_0 = frb_block[:,frb_loc - nsamps_in_mask:frb_loc]
                 frb_ex_1 = frb_block[:,frb_loc - nsamps_in_mask+1:frb_loc+1]
-                #print("frb_ex.shape = {0}, dm_mask.shape = {1}".format(frb_ex.shape, dm_mask.shape))
-                cross_p_m1 = frb_ex_m1 * dm_mask
-                cross_p_0 = frb_ex_0 * dm_mask
-                cross_p_1 = frb_ex_1 * dm_mask
+                frb_ex_1_vg = frb_block[:,frb_loc - nsamps_in_mask_vg+1:frb_loc+1]
+                
+                cross_p_1 = frb_ex_1 * dm_mask / np.sqrt(dm_mask.sum())
+                cross_p_1_vg = frb_ex_1_vg * dm_mask_vg / np.sqrt(dm_mask_vg.sum())
                 
                 
-                sum_of_cross_p_m1 = np.sum(cross_p_m1)
-                sum_of_cross_p_0 = np.sum(cross_p_0)
                 sum_of_cross_p_1 = np.sum(cross_p_1)
+                sum_of_cross_p_1_vg = np.sum(cross_p_1_vg)
                 
                 true_sums.append(np.sum(frb_block))
 
-                sums_m1.append(sum_of_cross_p_m1)
-                sums_0.append(sum_of_cross_p_0)
                 sums_1.append(sum_of_cross_p_1)
+                sums_1_vg.append(sum_of_cross_p_1_vg)
                 print(f"{ii}")
                 if args.plot_all:
                     plt.figure()
                     plt.imshow(dm_mask, aspect='auto', interpolation="None", alpha=0.5)
                     plt.imshow(frb_ex_1, aspect='auto', interpolation="None", alpha=0.5)
+                    plt.imshow(frb_ex_1_vg, aspect='auto', interpolation="None", alpha=0.5)
                     #plt.title("DM mask for dm_pccc = {0}, dm_samps = {1}, block = {2}".format(dm_pccc, dm_samps, ii))
                     plt.title("DM mask for dm_samps = {1}, block = {2}".format(dm_samps, dm_samps, ii))
             
                 if args.plot_all:
                     plt.figure()
                     plt.imshow(cross_p_1, aspect='auto', interpolation="None")
-                    plt.title("Common part of the sweep")
+                    plt.title("Common part of the sweep (Keith)")
+                    
+                    plt.figure()
+                    plt.imshow(cross_p_1_vg, aspect='auto', interpolation="None")
+                    plt.title("Common part of the sweep (VG)")
                 if args.plot_all: 
                     plt.figure()
                     plt.imshow(np.abs(iblock).sum(axis=0), aspect='auto', interpolation='None')
@@ -162,11 +171,10 @@ def main(args):
         pass
         
     plt.figure()
-    plt.plot(dms, np.array(sums_m1) /np.array(true_sums) , label="-1")
-    plt.plot(dms, np.array(sums_0) /np.array(true_sums) , label='0')
-    plt.plot(dms, np.array(sums_1) /np.array(true_sums) , label='+1')
+    plt.plot(dms, np.array(sums_1) /np.array(true_sums) , label='K')
+    plt.plot(dms, np.array(sums_1_vg) /np.array(true_sums) , label='V')
     plt.xlabel("DM [samps]")
-    plt.ylabel("Sum of frb sweep * FDMT dm mask")
+    plt.ylabel("Sum of frb sweep * FDMT dm mask / sqrt(sum(mask))")
     plt.legend()
     
     plt.show()
